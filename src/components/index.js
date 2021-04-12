f1e2c5d8e3026fa35c5bb047d41cfbb2c867ba4e
Juan Convers <me@jco.im>
2021-04-12 18:51:31 +0200

    Exposing CSV and JSON functions in the main index.

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,16 @@
 import Table from './Table'
 import PivotCsv from './PivotCsv'
+import PivotJSON from './PivotJSON'
 import PivotTable from './PivotTable'
 import PivotTableBarChart from './PivotTableBarChart'
+import {getPivotCsvData, getPivotJonsData} from './utils/pivotMain'
 
 export {
   Table,
   PivotCsv,
+  PivotJSON,
   PivotTable,
-  PivotTableBarChart
+  PivotTableBarChart,
+  getPivotCsvData,
+  getPivotJonsData
 }
