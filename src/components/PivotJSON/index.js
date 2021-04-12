eb653f7bbffaf9adb1fefec151961109bff1aa0b
Juan Convers <me@jco.im>
2021-04-12 18:18:59 +0200

    Adding PivotJSON component.

---
+++ src/components/PivotJSON/index.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+
+import { getJsonData } from '../utils/pivotMain'
+
+export default function PivotJSON ({
+  data,
+  filters,
+  rows,
+  columns,
+  columnsLabels,
+  values,
+  postprocessfn,
+  showColumnTotals,
+  showRowsTotals
+}) {
+  const [JSONData, setJSONData] = useState()
+
+  useEffect(() => {
+    const JSONContents = getJsonData({
+      data,
+      filters,
+      rows,
+      values,
+      columnsLabels,
+      postprocessfn,
+      showColumnTotals
+    })
+    setJSONData(JSONContents)
+  }, [data, rows, values, columnsLabels]) // eslint-disable-line
+
+  return (
+    <div>
+      {JSONData && <textarea style={{ width: '100%', height: '500px' }} value={
+        JSON.stringify(JSONData, null, 4)} readOnly />}
+    </div>
+  )
+}
+
+PivotJSON.propTypes = {
+  data: PropTypes.array,
+  rows: PropTypes.array,
+  columns: PropTypes.array,
+  columnsLabels: PropTypes.array,
+  values: PropTypes.array,
+  filters: PropTypes.array,
+  height: PropTypes.number,
+  postprocessfn: PropTypes.func,
+  showColumnTotals: PropTypes.bool,
+  showRowsTotals: PropTypes.bool,
+  width: PropTypes.number
+}
---
+++ src/components/PivotJSON/index.stories.js
@@ -0,0 +1,121 @@
+import React from 'react'
+
+import PivotJSON from './index'
+
+import testData from '../../testData/'
+
+export default {
+  title: 'PivotJSON',
+  component: PivotJSON
+}
+
+const Template = ({
+  data,
+  filters,
+  rows,
+  columns,
+  columnsLabels,
+  width,
+  values,
+  height,
+  postprocessfn,
+  showColumnTotals,
+  showRowsTotals
+}) =>
+  <PivotJSON
+    data={data}
+    columns={columns}
+    rows={rows}
+    columnsLabels={columnsLabels}
+    filters={filters}
+    height={height}
+    values={values}
+    width={width}
+    postprocessfn={postprocessfn}
+    showColumnTotals={showColumnTotals}
+    showRowsTotals={showRowsTotals}
+  />
+
+export const Default = Template.bind({})
+Default.args = {
+  data: testData,
+  rows: ['continent', 'country'],
+  values: [
+    { field: 'population', aggregator: 'sum' }
+  ]
+}
+
+export const ColumnLabelsAndFormatters = Template.bind({})
+ColumnLabelsAndFormatters.args = {
+  data: testData,
+  rows: ['continent', 'currency_code', 'government', 'country'],
+  columnsLabels: ['Continent', 'Currency', 'Government', 'Country', 'Population Sum', 'Count'],
+  values: [
+    {
+      field: 'population',
+      aggregator: 'sum',
+      formatter: x => Math.round(x).toLocaleString()
+    },
+    { field: 'area' }
+  ]
+}
+
+export const AverageAggregator = Template.bind({})
+AverageAggregator.args = {
+  data: testData,
+  rows: ['continent'],
+  columnsLabels: ['Continent', 'Population Avg'],
+  values: [
+    {
+      field: 'population',
+      aggregator: 'avg',
+      formatter: x => Math.round(x).toLocaleString()
+    }
+  ]
+}
+
+export const MedianAggregator = Template.bind({})
+MedianAggregator.args = {
+  data: testData,
+  rows: ['continent'],
+  columnsLabels: ['Continent', 'Population Avg'],
+  values: [
+    {
+      field: 'population',
+      aggregator: 'median',
+      formatter: x => Math.round(x).toLocaleString()
+    }
+  ]
+}
+
+export const CountAggregator = Template.bind({})
+CountAggregator.args = {
+  data: testData,
+  rows: ['continent'],
+  columnsLabels: ['Continent', 'Country Count'],
+  values: [
+    {
+      field: 'country'
+    }
+  ]
+}
+
+export const CustomFormatters = Template.bind({})
+CustomFormatters.args = {
+  data: testData,
+  rows: ['continent', 'country'],
+  columnsLabels: ['Continent', 'country', 'Population Sum', 'Area Sum'],
+  showColumnTotals: true,
+  values: [
+    {
+      field: 'population',
+      aggregator: 'sum',
+      formatter: x => `${parseFloat(x / 1000000).toFixed(2)} M. hab.`
+    },
+    {
+      field: 'area',
+      aggregator: 'sum',
+      formatter: x => `${Math.round(x).toLocaleString()} km²`
+    }
+  ]
+}
