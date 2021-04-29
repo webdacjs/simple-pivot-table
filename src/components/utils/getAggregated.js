4df7a3f502a8c59cd0c0f1b9ea9cf4ffba9f0ddb
Juan Convers <me@jco.im>
2021-04-29 10:42:17 +0200

    Moving getAggregated to separate component.

---
+++ src/components/utils/getAggregated.js
@@ -0,0 +1,37 @@
+export function getNumericValue (value) {
+  const numValue = parseFloat(value)
+  if (isNaN(numValue)) {
+    return 0
+  }
+  return numValue
+}
+
+export function getReducedValue (values, aggregator, formatter) {
+  if (aggregator === 'sum') {
+    const rawValue = values.reduce((a, b) => a + (getNumericValue(b) || 0), 0)
+    return formatter ? formatter(rawValue) : rawValue
+  }
+  if (aggregator === 'avg') {
+    const rawValue = values.reduce((a, b) => a + (getNumericValue(b) || a), 0) / values.length
+    return formatter ? formatter(rawValue) : rawValue
+  }
+  if (aggregator === 'median') {
+    const rawValue = values[Math.round(values.length / 2)]
+    return formatter ? formatter(rawValue) : rawValue
+  }
+  if (typeof aggregator === 'function') {
+    const rawValue = values.reduce((a, b) => aggregator(a, b))
+    return formatter ? formatter(rawValue) : rawValue
+  }
+  // default count
+  return values.length
+}
+
+export default function getAggregatedValues (items, vals, postprocessfn) {
+  const reduced = {}
+  vals.forEach(val => {
+    const values = items.map(item => item[val.field])
+    reduced[val.field] = getReducedValue(values, val.aggregator, val.formatter)
+  })
+  return postprocessfn ? postprocessfn(reduced) : reduced
+}
---
+++ src/components/utils/getAggregated.test.js
@@ -0,0 +1,45 @@
+import {
+  getNumericValue,
+  getReducedValue
+} from './getAggregated'
+
+test('Testing the getNumericValue function', () => {
+  const fromStr = getNumericValue('30')
+  const fromNumber = getNumericValue(30)
+  const noNumber = getNumericValue('juan')
+  expect(fromStr).toBe(30)
+  expect(fromNumber).toBe(30)
+  expect(noNumber).toBe(0)
+})
+
+test('Testing the def count reducer', () => {
+  const testArray = [5, 10, 15]
+  const countReduced = getReducedValue(testArray)
+  expect(countReduced).toBe(3)
+})
+
+test('Testing the sum reducer', () => {
+  const testArray = [5, 10, 15]
+  const sumReduced = getReducedValue(testArray, 'sum')
+  expect(sumReduced).toBe(30)
+})
+
+test('Testing the avg reducer', () => {
+  const testArray = [5, 10, 15]
+  const avgReduced = getReducedValue(testArray, 'avg')
+  expect(avgReduced).toBe(10)
+})
+
+test('Testing the custom reducer (min)', () => {
+  const customAg = (a, b) => (!a || b < a) ? b : a
+  const testArray = [5, 10, 15]
+  const minReduced = getReducedValue(testArray, customAg)
+  expect(minReduced).toBe(5)
+})
+
+test('Testing the custom reducer (max)', () => {
+  const customAg = (a, b) => (!a || b > a) ? b : a
+  const testArray = [5, 10, 15]
+  const maxReduced = getReducedValue(testArray, customAg)
+  expect(maxReduced).toBe(15)
+})
