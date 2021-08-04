8f2f48d6bae5d4f19ac2f55259fe31c0d23edd4f
Juan Convers <me@jco.im>
2021-08-04 18:47:17 +0200

    Switching the import / export order of the non-react modules.

--- src/components/index.js
+++ src/components/index.js
@@ -1,18 +1,18 @@
+import { getPivotCsvData, getPivotJsonData } from './utils/pivotMain'
 import Table from './Table'
 import PivotCsv from './PivotCsv'
 import PivotJSON from './PivotJSON'
 import PivotTable from './PivotTable'
 import ExperimentalPivotTable from './ExperimentalPivotTable'
 import PivotTableBarChart from './PivotTableBarChart'
-import { getPivotCsvData, getPivotJsonData } from './utils/pivotMain'
 
-export {
+export {  
+  getPivotCsvData,
+  getPivotJsonData,
   Table,
   PivotCsv,
   PivotJSON,
   ExperimentalPivotTable,
   PivotTable,
-  PivotTableBarChart,
-  getPivotCsvData,
-  getPivotJsonData
+  PivotTableBarChart
 }
